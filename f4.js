4e2b019ec4207c5f05034cd5c3a72c653a60d1c2
Ekaterina Kopylova <kat_kap@mail.ru>
2022-12-24 13:38:13 +0300

    Add files via upload

--- f4.js
+++ f4.js
@@ -1,16 +1,30 @@
+
 function showNumbers(a, b) {
     let currentNum = a;
     let timerID = setInterval(function() {
         console.log(currentNum);
-        currentNum++;
-        if (currentNum > b) {
+        if (currentNum >= b) {
             clearInterval(timerID);
-        } 
-        
+        } else {
+            currentNum++;
+        }   
     }, 1000);
 }
 
-showNumbers(-7, 1);
+// 2ой способ с использованием рекурсивного вызова
+// function showNumbers(a, b) {
+    // let currentNum = a;  
+    // setTimeout(function go() {
+    //   console.log(currentNum);
+    //   if (currentNum < b) {
+        // setTimeout(go, 1000);
+    //   }
+    //   currentNum++;
+    // }, 1000);
+// }
+
+
+showNumbers(-5, 1);
 
 
 
